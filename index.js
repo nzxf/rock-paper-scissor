51b7a7872c82b397e1a3a1a38c25f02ac52fd88e
nzxf <122195155+nzxf@users.noreply.github.com>
2023-01-18 23:24:01 +0700

    Translate computer random number into rock/paper/scissor

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 
 //computer's choices randomizer//
 const getComputerChoice = (Math.floor(Math.random() * 3) + 1)
+
+//translate computer's choice into rock/paper/scissor //
+if (getComputerChoice === 1) {
+    console.log("Computer chose ROCK")
+} else if (getComputerChoice === 2) {
+    console.log("Computer chose PAPER")
+} else if (getComputerChoice === 3) {
+    console.log("Computer chose SCISSOR")
+}
