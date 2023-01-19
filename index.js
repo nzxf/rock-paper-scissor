fd4a672fa9108480d05aa947cdd15e7712bfdd1e
nzxf <122195155+nzxf@users.noreply.github.com>
2023-01-19 12:29:25 +0700

    Add filtered and case- insensitive user input

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ if (getComputerChoice === 1) {
     console.log("Computer chose SCISSOR")
 }
 
-
 //randomizer outliner tester//
 //(if any number outside 1,2, and 3 was chosen, this message will be viewed)//
 else {
     console.log("Computer accidentally hit its own head and fainted")
+}
+
+//user's choice input// 
+let userChoice = prompt("What do you choose? Rock, Paper or Scissor?")
+
+//insensitive case input//
+if (userChoice.toLowerCase() === "rock") {
+    console.log("You chose ROCK")
+} else if (userChoice.toLowerCase() === "paper") {
+    console.log("You chose PAPER")
+} else if (userChoice.toLowerCase() === "scissor") {
+    console.log("You chose SCISSOR")
+}
+
+//to filter user input (only accept 'rock', 'paper' or 'scissor')//
+
+else {
+    console.log("Your input is invalid. Choose Rock/Paper/Scissor!!")
 }
