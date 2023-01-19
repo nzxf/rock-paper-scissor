9ef6815d834de9f9b8d833df0574efdfc24a6342
nzxf <122195155+nzxf@users.noreply.github.com>
2023-01-19 10:50:43 +0700

    Add random number checker

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ if (getComputerChoice === 1) {
 } else if (getComputerChoice === 3) {
     console.log("Computer chose SCISSOR")
 }
+
+
+//randomizer outliner tester//
+//(if any number outside 1,2, and 3 was chosen, this message will be viewed)//
+else {
+    console.log("Computer accidentally hit its own head and fainted")
+}
