b1eb2e5fbca26f32a79ca978f6bc50c069f0eabc
nzxf <122195155+nzxf@users.noreply.github.com>
2023-01-22 16:02:45 +0700

    Simplify how the computer get random hand

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+//game requirement//
+const options = ['rock', 'paper', 'scissor'];
 
-//computer's choices randomizer//
-const getComputerChoice = (Math.floor(Math.random() * 3) + 1)
-
-//translate computer's choice into rock/paper/scissor //
-if (getComputerChoice === 1) {
-    console.log("Computer chose ROCK")
-} else if (getComputerChoice === 2) {
-    console.log("Computer chose PAPER")
-} else if (getComputerChoice === 3) {
-    console.log("Computer chose SCISSOR")
+//computer random choice//
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * parseInt(options.length));
+    return options[random];
 }
 
 //randomizer outliner tester//
